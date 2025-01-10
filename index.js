f5e3ad474ac590a0a6b17909f9a81fd900b84d44
Ekan <ekanessien@gmail.com>
2025-01-09 22:12:37 -0800

    Functions and Event Listeners Advances

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const video = document.getElementById("video");
-const plary = document.getElementById("play");
+const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
 
+//Play and Pause Video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
+//Update Play / Pause Icon
+function updatePlayIcon() {
+    if(video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
 
+//Update Progress & Timestamp
+function updateProgress() {
+    return true;
+}
 
+//Set Video Time to Progress
+function setVideoProgress() {
+    return true;
+}
 
+//Stop Video
+function stopVideo() {
+    return true; 
+}
 
 //Event Listeners
 video.addEventListener("click", toggleVideoStatus);
@@ -15,7 +43,7 @@ video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
 video.addEventListener("timeupdate", updateProgress);
 
-plary.addEventListener("click", toggleVideoStatus);
+play.addEventListener("click", toggleVideoStatus);
 
 stop.addEventListener("click", stopVideo);
 
